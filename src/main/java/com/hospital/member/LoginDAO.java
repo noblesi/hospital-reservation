edf9fa1aa6d0b12676dd8b1327d9c0b466087785
parkjeungwook <ddoddo1213@gmail.com>
2026-06-15 17:50:27 +0900

    loginDAO

--- src/main/java/com/hospital/member/LoginDAO.java
+++ src/main/java/com/hospital/member/LoginDAO.java
@@ -1,12 +1,29 @@
 package com.hospital.member;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.hospital.common.DBConnection;
 import com.hospital.common.MemberDTO;
 
 /**
  * loginDAO 
  */
 public class LoginDAO {
-
+	
+	private static LoginDAO lDAO;
+	
+	private LoginDAO() {
+		
+	}//LoginDAO
+	
+	public static LoginDAO getInstance() {
+		if(lDAO == null) {
+			lDAO = new LoginDAO();
+		}//end if
+		return lDAO;
+	}//LoginDAO
 	
 	/**
 	 * 아이디로 멤버 정보 
@@ -14,7 +31,11 @@ public class LoginDAO {
 	 * @return 멤버DTO를 반환 
 	 */
 	public MemberDTO selectMemberByLoginId(String loginId) {
-	
+		
+		DBConnection dbcon = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		return null;
 	}//selectMemberByLoginId
 }//loginDAO
