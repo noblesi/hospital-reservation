c5c1f064a5b2b35c2e327db8a3d3792ad67e2779
parkjeungwook <ddoddo1213@gmail.com>
2026-06-15 17:20:23 +0900

    loginDAO, loginService

---
+++ src/main/java/com/hospital/member/LoginDAO.java
@@ -0,0 +1,20 @@
+package com.hospital.member;
+
+import com.hospital.common.MemberDTO;
+
+/**
+ * loginDAO 
+ */
+public class LoginDAO {
+
+	
+	/**
+	 * 아이디로 멤버 정보 
+	 * @param loginId 아이디를 받아서 
+	 * @return 멤버DTO를 반환 
+	 */
+	public MemberDTO selectMemberByLoginId(String loginId) {
+	
+		return null;
+	}//selectMemberByLoginId
+}//loginDAO
---
+++ src/main/java/com/hospital/member/LoginService.java
@@ -0,0 +1,12 @@
+package com.hospital.member;
+
+import com.hospital.common.MemberDTO;
+
+public class LoginService {
+	private LoginDAO loginDAO;
+	
+	public MemberDTO login(String loginId, String password) {
+		
+		return null;
+	}//login 
+}//loginService
