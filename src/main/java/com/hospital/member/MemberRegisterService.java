364883bd65f1c78fab6cd3c752b692f1ea2a1b5b
parkjeungwook <ddoddo1213@gmail.com>
2026-06-15 17:20:47 +0900

    MemberRegisterService

---
+++ src/main/java/com/hospital/member/MemberRegisterService.java
@@ -0,0 +1,20 @@
+package com.hospital.member;
+
+import com.hospital.common.MemberDTO;
+import com.hospital.common.MinorMemberDTO;
+
+public class MemberRegisterService {
+
+	public boolean checkLoginIdDuplicate(String loginId) {
+		
+		return false;
+	}//checkLoginIdDuplicate
+	
+	public boolean registerMember(MemberDTO mDTO, MinorMemberDTO minorDTO) {
+		return false;
+	}//registerMember
+	
+	public MemberDTO searchRegiSter(String LoginId) {
+		return null;
+	}//searchRegiSter
+}//MemberRegisterService
