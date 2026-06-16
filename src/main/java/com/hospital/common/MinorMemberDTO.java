1db0ee37b8b7cc1c728f4bb8173ac0d6bcf0ee6b
parkjeungwook <ddoddo1213@gmail.com>
2026-06-16 23:21:34 +0900

    오타 수정

--- src/main/java/com/hospital/common/MinorMemberDTO.java
+++ src/main/java/com/hospital/common/MinorMemberDTO.java
@@ -1,5 +1,7 @@
 package com.hospital.common;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,12 +10,12 @@ import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
 @Setter
+@Getter
 @ToString
 public class MinorMemberDTO {
 	private String patientNo;
 	private String relationship;
 	private String minorName;
-	private String minorBirthDate;
+	private Date minorBirthDate;
 }//MinorMemberDTO
