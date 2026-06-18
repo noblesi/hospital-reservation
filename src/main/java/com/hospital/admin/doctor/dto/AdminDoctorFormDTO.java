f6a021b707ddf13b3287f778d11a04df5718ee53
ChaeDaeKyung <enemy@hanmail.net>
2026-06-18 23:36:30 +0900

    AdminDoctorFormDTO,OptionDTO,SearchDTO 업로드

---
+++ src/main/java/com/hospital/admin/doctor/dto/AdminDoctorFormDTO.java
@@ -0,0 +1,37 @@
+package com.hospital.admin.doctor.dto;
+
+import java.util.List;
+
+import com.hospital.common.dto.DepartmentDTO;
+import com.hospital.common.dto.DoctorCareerDTO;
+import com.hospital.common.dto.DoctorDTO;
+import com.hospital.common.dto.DoctorEducationDTO;
+import com.hospital.common.dto.DoctorPositionDTO;
+import com.hospital.common.dto.DoctorScheduleDTO;
+import com.hospital.common.dto.DoctorStatusDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AdminDoctorFormDTO {
+	
+	private DoctorDTO doctorDTO;
+	private List<DoctorScheduleDTO> scheduleList;
+	private List<DoctorEducationDTO> educationList;
+	private List<DoctorCareerDTO> careerList;
+	private List<DepartmentDTO> departmentList;
+	private List<DoctorPositionDTO> positionList;
+	private List<DoctorStatusDTO> statusList;
+	private String porfileImageFileName;
+	private String detailImageFileName;
+	
+}//class
