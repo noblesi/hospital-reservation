e86830f0b52c9a1646c89de3c6181fcd99466dd0
ChaeDaeKyung <enemy@hanmail.net>
2026-06-18 16:37:14 +0900

    adminDepartmentDAO.java 작업중

--- src/main/java/com/hospital/admin/department/AdminDepartmentDAO.java
+++ src/main/java/com/hospital/admin/department/AdminDepartmentDAO.java
@@ -1,14 +1,18 @@
 package com.hospital.admin.department;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
 import com.hospital.admin.department.dto.AdminDepartmentSearchDTO;
+import com.hospital.common.DBConnection;
 import com.hospital.common.dto.DepartmentDTO;
 
 
-public class AdminDepartmentDAO {
+public class AdminDepartmentDAO{
 	private static AdminDepartmentDAO adminDepartmentDAO;
-	
 	private AdminDepartmentDAO() {
 	}//AdminDepartmentDAO
 	
@@ -21,14 +25,67 @@ public class AdminDepartmentDAO {
 	
 //	public int selectDepartmentTotalCnt(AdminDepartmentSearchDTO searchDTO) {
 	public int selectDepartmentTotalCnt() {
-		AdminDepartmentSearchDTO adminDepartmentSearchDTO = null;
+		/* 사용하는 건지는 잘 모름 */
+		//AdminDepartmentSearchDTO adminDepartmentSearchDTO = null;
 		
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		int totalCnt = 0;
+		
+		String selectSql="select count(*) from department";
+		
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(selectSql);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				totalCnt=rs.getInt(0);
+			}// end if
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(rs,pstmt,conn);
+		}// end try catch
+		
+		return totalCnt;
 	}// selectDepartmentTotalCnt
 	
 	public List<AdminDepartmentSearchDTO> selectDepartmentList(AdminDepartmentSearchDTO searchDTO){
-		List<DepartmentDTO> departmentDTO = null;
-		List<AdminDepartmentSearchDTO> adminDepartmentSearchDTO = null;
+		List<DepartmentDTO> departmentDTOList = new ArrayList<DepartmentDTO>();
+		List<AdminDepartmentSearchDTO> adminDepartmentSearchDTO = new ArrayList<AdminDepartmentSearchDTO>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String selectSql="select * from department";
+		
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(selectSql);
+			rs = pstmt.executeQuery();
+			
+			DepartmentDTO departmentDTO = null;
+			
+			while(rs.next()) {
+				departmentDTO = new DepartmentDTO();
+				departmentDTO.setDeptNo(rs.getString("dept_no"));
+				departmentDTO.setDeptName(rs.getString("dept_name"));
+				departmentDTO.setDescription(rs.getString("description"));
+				departmentDTO.setDeptLoc(rs.getString("dept_loc"));
+				departmentDTO.setIsActiveYn(rs.getString("is_active_yn"));
+				departmentDTOList.add(departmentDTO);
+			}// end if
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(rs,pstmt,conn);
+		}// end try catch
 		
 		return adminDepartmentSearchDTO;
 	}//selectDepartmentList
