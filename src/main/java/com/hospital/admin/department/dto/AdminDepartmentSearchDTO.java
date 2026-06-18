e2b35d5b36afc5ef98802918f596ba990bf73985
ChaeDaeKyung <enemy@hanmail.net>
2026-06-18 23:33:46 +0900

    AdminDepartmentDTO,DAO,Service 업로드

---
+++ src/main/java/com/hospital/admin/department/dto/AdminDepartmentSearchDTO.java
@@ -0,0 +1,24 @@
+package com.hospital.admin.department.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AdminDepartmentSearchDTO {
+
+	private int startNum;
+	private int endNum;
+	private String field;
+	private String keyword;
+	private String isActiveYn;
+	
+	
+}// class
