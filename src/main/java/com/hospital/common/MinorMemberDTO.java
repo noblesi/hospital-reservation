75aefd0ad3bb89802f2b197e36dc6e07908009f5
parkjeungwook <ddoddo1213@gmail.com>
2026-06-22 16:05:26 +0900

    DTO 주석 및 코드 정리

--- src/main/java/com/hospital/common/MinorMemberDTO.java
+++ src/main/java/com/hospital/common/MinorMemberDTO.java
@@ -8,14 +8,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 보호자 회원에게 연결된 미성년자 회원 정보를 전달하는 DTO
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 @ToString
 public class MinorMemberDTO {
+	/** 보호자 회원과 연결되는 환자번호 */
 	private String patientNo;
+	/** 보호자와 미성년자의 관계 */
 	private String relationship;
+	/** 미성년자 이름 */
 	private String minorName;
+	/** 미성년자 생년월일 */
 	private Date minorBirthDate;
 }//MinorMemberDTO
